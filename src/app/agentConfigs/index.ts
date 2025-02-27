855d3bb8ea41e57c4beca3ed83c7001c2cdb31f0
ChatSites™ <info@chatsites.io>
2025-02-26 17:35:24 -0700

    Update index.ts

--- src/app/agentConfigs/index.ts
+++ src/app/agentConfigs/index.ts
@@ -1,7 +1,22 @@
 import { AllAgentConfigsType } from "@/app/types";
-import frontDeskAuthentication from "./frontDeskAuthentication";
-import customerServiceRetail from "./customerServiceRetail";
-import simpleExample from "./simpleExample";
+import realEstateAgents from "./realEstateAgents";
+import ecomAgents from "./ecomAgents";
+import healthcareAgents from "./healthcareAgents";
+import financeAgents from "./financeAgents";
+import educationAgents from "./educationAgents";
+
+// src/app/agentConfigs/index.ts
+
+// Define agents by industry (Scenario)
+export const allAgentSets: AllAgentConfigsType = {
+  "Real Estate": realEstateAgents,
+  "E-commerce": ecomAgents,
+  "Healthcare": healthcareAgents,
+  "Finance": financeAgents,
+  "Education": educationAgents,
+};
+
+export const defaultAgentSetKey = Object.keys(allAgentSets)[0];
 
 
 // src/app/agentConfigs/index.ts
